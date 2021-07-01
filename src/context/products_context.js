a8ec52c098fd83e8210310bd9d29c932c41b667f
RodCerB <rodrigo.cbraga@hotmail.com>
2021-07-01 16:45:48 -0300

    estruturando o context e reducer do sidebar

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/products_reducer'
 import { products_url as url } from '../utils/constants'
+// Nesse projeto decidido deixar todas as ações já pré-programadas como de ver abaixo. Apenas uma facilitada para adiantar na produção, mas poderia no dispatch e em type usar string diretamente que nao haveria problema
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -13,13 +14,25 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
-const initialState = {}
+const initialState = {
+  isSidebarOpen:false
+}
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
+  // Considerando a grandeza desse projeto, foi decidido usar o usereducer para facilitar o crescimento do projeto. 
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const openSidebar = () =>{
+    dispatch({type:SIDEBAR_OPEN})
+  }
+  const closeSidebar = () =>{
+    dispatch({type:SIDEBAR_CLOSE})
+  }
+
   return (
-    <ProductsContext.Provider value='products context'>
+    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
       {children}
     </ProductsContext.Provider>
   )
